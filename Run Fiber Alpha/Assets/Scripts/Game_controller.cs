3a3fc91679059cd149c7a2734fede9eef6083966
oscarg97 <guachicali_oscarg@hotmail.com>
2017-05-27 11:43:00 -0700

    Subido

---
+++ Run Fiber Alpha/Assets/Scripts/Game_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_controller : MonoBehaviour {
+
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public Image platform1;
+    public Image platform2;
+    public Image platform3;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float finalSpeed = 1;
+	}
+}
