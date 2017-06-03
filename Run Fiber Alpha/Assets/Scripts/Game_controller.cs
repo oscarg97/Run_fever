2d340520fde760623db0e98adb552d7e51a371e6
oscarg97 <guachicali_oscarg@hotmail.com>
2017-06-03 13:21:25 -0700

    Lo que hay

--- Run Fiber Alpha/Assets/Scripts/Game_controller.cs
+++ Run Fiber Alpha/Assets/Scripts/Game_controller.cs
@@ -7,16 +7,34 @@ public class Game_controller : MonoBehaviour {
 
     public float parallaxSpeed = 0.02f;
     public RawImage background;
-    public Image platform1;
-    public Image platform2;
-    public Image platform3;
+    public RawImage platform1;
+    public RawImage platform2;
+    public RawImage platform3;
+	public RawImage platform11;
+	public RawImage platform21;
+    public enum GameState { Idle, Playing };
+	private GameState gameState = GameState.Idle;
     // Use this for initialization
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float finalSpeed = 1;
+	public void Update () {
+		//Empieza
+		if ((Input.GetKeyDown ("up") || Input.GetMouseButtonDown (0))) {
+			gameState = GameState.Playing;
+		} else if (gameState == GameState.Playing) {
+			parallax (background);
+			parallax (platform1);
+			parallax (platform11);
+			parallax (platform2);
+			parallax (platform21);
+			parallax (platform3);
+		}
+	}
+	void parallax (RawImage image){
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		image.uvRect = new Rect (image.uvRect.x + finalSpeed, 0f, 1f, 1f);
 	}
 }
---
+++ Run Fiber Alpha/Assets/Scripts/Start.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Start : MonoBehaviour {
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public enum GameState { Idle, Playing };
+	public GameState gameState = GameState.Idle;
+	Game_controller gc = new Game_controller ();
+	// Use this for initialization
+	void Start1 () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+		if (Input.GetKeyDown ("up") || Input.GetMouseButtonDown (0)) {
+			gameState = GameState.Playing;
+			
+		} else if (gameState == GameState.Playing) {
+			gc.Update ();
+		}
+	}
+}
